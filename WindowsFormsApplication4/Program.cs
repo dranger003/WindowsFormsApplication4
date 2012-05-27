352c7a6a5615baf197238f45922f18b0db984a1c
DAN™ <dranger003@gmail.com>
2012-05-26 20:20:15 -0400

    Initial commit

---
+++ WindowsFormsApplication4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+using ClassLibrary1;
+
+namespace WindowsFormsApplication4
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            RegistrationServices rs = new RegistrationServices();
+            int cookie = rs.RegisterTypeForComClients(typeof(Class1), RegistrationClassContext.LocalServer, RegistrationConnectionType.MultipleUse);
+
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            catch
+            {
+            }
+
+            rs.UnregisterTypeForComClients(cookie);
+        }
+    }
+}
---
+++ WindowsFormsApplication4/TrayForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class TrayForm : Form
+    {
+        private enum ToggleMode { Show, Hide };
+        private FormWindowState _previousState;
+
+        public TrayForm()
+        {
+            InitializeComponent();
+
+            Font = new Font("MS Shell Dlg 2", Font.Size);
+
+            if (DesignMode || Process.GetCurrentProcess().ProcessName == "devenv")
+            {
+                notifyIcon.Visible = false;
+            }
+            else
+            {
+                StartPosition = FormStartPosition.CenterScreen;
+
+                Load += new EventHandler(TrayForm_Load);
+                SizeChanged += new EventHandler(TrayForm_SizeChanged);
+                FormClosing += new FormClosingEventHandler(TrayForm_FormClosing);
+
+                ContextMenu contextMenu = new ContextMenu(
+                    new MenuItem[]
+                {
+                    new MenuItem(String.Empty, new EventHandler(ToggleForm)),
+                    new MenuItem("&Quit", new EventHandler(Quit))
+                }
+                );
+
+                contextMenu.MenuItems[0].DefaultItem = true;
+                contextMenu.Popup += new EventHandler(contextMenu_Popup);
+
+                notifyIcon.ContextMenu = contextMenu;
+                notifyIcon.DoubleClick += new EventHandler(contextMenu_Popup);
+
+                // Start hidden
+                Visible = false;
+            }
+        }
+
+        private void contextMenu_Popup(object sender, EventArgs e)
+        {
+            ContextMenu contextMenu = null;
+
+            if (sender is ContextMenu)
+                contextMenu = (ContextMenu)sender;
+            else if (sender is NotifyIcon)
+                contextMenu = ((NotifyIcon)sender).ContextMenu;
+
+            if (contextMenu != null)
+            {
+                contextMenu.MenuItems[0].Text = WindowState == FormWindowState.Minimized ? "&Show" : "&Hide";
+                contextMenu.MenuItems[0].Tag = WindowState == FormWindowState.Minimized ? ToggleMode.Show : ToggleMode.Hide;
+            }
+
+            if (sender is NotifyIcon)
+                ToggleForm(sender, new EventArgs());
+        }
+
+        private void TrayForm_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void TrayForm_SizeChanged(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+                TrayForm_AfterMinimized(sender, e);
+        }
+
+        private void TrayForm_BeforeMinimized(object sender, EventArgs e)
+        {
+            _previousState = WindowState;
+        }
+
+        private void TrayForm_AfterMinimized(object sender, EventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(
+                new WaitCallback(
+                    delegate(object state1)
+                    {
+                        Thread.Sleep(250);
+
+                        BeginInvoke(
+                            new WaitCallback(
+                                delegate(object state2)
+                                {
+                                    Visible = false;
+                                }
+                            ),
+                            new object[] { null }
+                        );
+                    }
+                )
+            );
+        }
+
+        private void TrayForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+
+                _previousState = WindowState;
+                WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        private void ToggleForm(object sender, EventArgs e)
+        {
+            switch ((ToggleMode)notifyIcon.ContextMenu.MenuItems[0].Tag)
+            {
+                case ToggleMode.Show:
+                    Visible = true;
+
+                    ThreadPool.QueueUserWorkItem(
+                        new WaitCallback(
+                            delegate(object state1)
+                            {
+                                Thread.Sleep(250);
+
+                                BeginInvoke(
+                                    new WaitCallback(
+                                        delegate(object state2)
+                                        {
+                                            WindowState = _previousState;
+                                        }
+                                    ),
+                                    new object[] { null }
+                                );
+                            }
+                        ),
+                        null
+                    );
+                    break;
+                case ToggleMode.Hide:
+                    TrayForm_BeforeMinimized(this, new EventArgs());
+                    WindowState = FormWindowState.Minimized;
+                    break;
+            }
+        }
+
+        private void Quit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x0112: // WM_SYSCOMMAND
+                    if ((m.WParam.ToInt32() & 0xfff0) == 0xf020) // SC_MINIMIZE
+                        TrayForm_BeforeMinimized(this, new EventArgs());
+
+                    break;
+            }
+
+            base.WndProc(ref m);
+        }
+    }
+}
